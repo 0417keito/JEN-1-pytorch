fd77ca7fdadd5a4bf594d7c54b7123acf39a0b18
0417keito <0417keito@gmail.com>
2023-12-05 21:51:59 +0900

    add angle_scheduler

--- jen1/noise_schedule.py
+++ jen1/noise_schedule.py
@@ -12,13 +12,14 @@ def get_beta_schedule(schedule_name, num_diffusion_timesteps):
         beta_start = scale * 0.0001
         beta_end = scale * 0.02
         return torch.linspace(
-            beta_start, beta_end, num_diffusion_timesteps, dtype=torch.float64
-        )
+            beta_start, beta_end, num_diffusion_timesteps), None
     elif schedule_name == "cosine":
         return betas_for_alpha_bar(
             num_diffusion_timesteps,
             lambda t: math.cos((t + 0.008) / 1.008 * math.pi / 2) ** 2,
         )
+    elif schedule_name == 'angle':
+        return angle_schedule(num_diffusion_timesteps)
     else:
         raise NotImplementedError(f"unknown beta schedule: {schedule_name}")
 
@@ -29,4 +30,12 @@ def betas_for_alpha_bar(num_diffusion_timesteps, alpha_bar, max_beta=0.999):
         t1 = i / num_diffusion_timesteps
         t2 = (i + 1) / num_diffusion_timesteps
         betas.append(min(1 - alpha_bar(t2) / alpha_bar(t1), max_beta))
-    return torch.tensor(betas)
+    return torch.tensor(betas), None
+
+def angle_schedule(num_diffusion_timesteps, vmax=1.0, vmin=0.0):
+    scale = 1000 / num_diffusion_timesteps
+    
+    t = (vmax - vmin) * torch.rand(num_diffusion_timesteps) + vmin
+    angle = t * math.pi / 2
+    alpha, beta = torch.cos(angle), torch.sin(angle)
+    return alpha, beta
