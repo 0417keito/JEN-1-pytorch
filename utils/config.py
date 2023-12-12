994f588b244f20ae1c2340cd7ab2421e8da7801b
0417keito <0417keito@gmail.com>
2023-12-12 14:31:38 +0900

    fix remove composer setting

--- utils/config.py
+++ utils/config.py
@@ -10,11 +10,11 @@ class DataConfig:
     # min_duration, max_duration, and sample_duration are all listed in seconds.
     min_duration = 0
     max_duration = 300
-    sample_duration = 32 # This length determines the length of the latent variable. Adjust the length of the latent variable so that it is 2**(num_layers).
+    sample_duration = 30 # This length determines the length of the latent variable. Adjust the length of the latent variable so that it is 2**(num_layers).
     aug_shift = True
-    batch_size = 1 
+    batch_size = 12 
     shuffle = True
-    train_test_split = 0.5
+    train_test_split = 0.8
     device = 'cuda' if torch.cuda.is_available else 'cpu'
 
 @dataclass
@@ -28,8 +28,6 @@ class GDM_Config:
     embedding_scale = 0.8
     batch_cfg = True
     scale_cfg = True
-    composer = False
-    demix_list = ['bass', 'drums', 'other', 'vocals']
 
 @dataclass
 class VDM_Config: 
@@ -39,7 +37,6 @@ class VDM_Config:
     embedding_scale = 0.8
     batch_cfg = True
     scale_cfg = True
-    composer = False
     
 @dataclass
 class DiffusionConfig:
@@ -95,7 +92,7 @@ class Config:
     eval_interval = 20
     is_fintuning = False
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    diffusion_type = 'vdm'
+    diffusion_type = 'gdm'
     dataset_config = DataConfig
     diffusion_config = DiffusionConfig
     model_config = ModelConfig
