02fb30fd15bc5b0ccd9763f07d98fcdbcefe6eb0
0417keito <0417keito@gmail.com>
2024-01-19 04:24:00 +0900

    preprocess.py create for preprocess func

---
+++ dataset/preprocess.py
@@ -0,0 +1,44 @@
+import os
+import torch
+import torchaudio
+
+def get_duration_sec(file): 
+        wav, sr = torchaudio.load(file)
+        duration_sec = wav.shape[-1] / sr
+        return duration_sec
+
+def filter(audio_files, durations):
+        keep = []
+        audio_files = []
+        for i in range(len(audio_files)):
+            filepath = audio_files[i]
+            if durations[i] < min_duration:
+                continue
+            if durations[i] >= max_duration:
+                continue
+            keep.append(i)
+            audio_files.append(filepath)
+        durations = [durations[i] for i in keep] # in (s)
+        duration_tensor = torch.tensor(durations)
+        cumsum = torch.cumsum(duration_tensor, dim=0) # in (s)
+        return cumsum 
+    
+def init_dataset(audio_files_dir, min_duration, max_duration):
+    audio_files = os.listdir(audio_files_dir)
+    audio_files = [f'{audio_files_dir}/{file}' for file in audio_files if file.endswith('.wav') or file.endswith('.mp3')]
+    durations = [get_duration_sec(file) for file in audio_files]
+    cumsum = filter(audio_files=audio_files, durations=durations, 
+                    min_duration=min_duration, max_duration=max_duration)
+    return cumsum, durations
+
+
+if __name__ == '__main__':
+    cumsum_path = ...
+    durations_path = ...
+    min_duration, max_duration = ... # please type min_duration, max_duration
+    audio_files_dir = 'please type your dir '
+    cumsum, durations = init_dataset(audio_files_dir=audio_files_dir, min_duration=min_duration,
+                                     max_duration=max_duration)
+    
+    torch.save(cumsum, cumsum_path)
+    torch.save(durations, durations_path)
